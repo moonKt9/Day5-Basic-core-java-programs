27db1fb3b7367ee22c96be2912fbcd9daa5d9996
Kritika <102981807+moonKt9@users.noreply.github.com>
2022-07-06 08:39:10 +0530

    Update PrimeFactor.java

--- PrimeFactor.java
+++ PrimeFactor.java
@@ -1,19 +1,43 @@
 package com.bridgelabz;
 import java.util.Scanner;
-public class PrimeFactor {
+
+
+public class PrimeFactor{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("Enter the number to find prime factors:");
-Scanner sc=new Scanner(System.in);
-int num=sc.nextInt();
-for(int i=1;i<=num/2;i++) {
-	if(num%i==0) {
-		System.out.println(i+" ");
-	}
 	
-}
-System.out.println(num);
+		System.out.println("Enter the number to find it's prime factors : ");
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		
+		System.out.println("Prime Factors are for "+input+" :");
+		
+		if (isPrime(input)) {
+			System.out.print(input);
+		} else {
+			for (int i = 1; i <= input/2 ; i++) {
+			
+				if(input % i == 0 && isPrime(i)) {
+					System.out.print(i+" ");
+				}
+			
+			}
+		}
+		sc.close();
+	}
+
+	public static boolean isPrime(int number) {
+		if(number == 1)
+			return false;
+		
+		for (int i = 2; i <= number/2 ; i++) {
+		
+			if ( number % i == 0 ) {
+				return false;
+			}
+		
+		}
+		return true;
 	}
 
 }
